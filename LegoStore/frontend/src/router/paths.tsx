07285f7ffe9662086dec2a8557f5daf6e934ec68
Dana Shafer <shafer.dana@gmail.com>
2025-11-27 14:42:46 +0200

    create layout and home files

---
+++ LegoStore/frontend/src/router/paths.tsx
@@ -0,0 +1,33 @@
+import type { JSX } from "react";
+import { Home } from "../views/Home";
+
+
+// import Layout from "../views/Layout";
+
+export interface Page {
+  path: string;
+  element: JSX.Element;
+  name: string;
+  isShown: boolean;
+}
+
+export const routes: Page[] = [
+  {
+    path: "/",
+    element: <Home />,
+    name: "בית",
+    isShown: true,
+  },
+  
+];
+
+export const paths = [
+  {
+    path: "/",
+    element: <Home />,
+    children: routes.map((route) => ({
+      path: route.path,
+      element: route.element,
+    })),
+  },
+];
