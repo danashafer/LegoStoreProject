10cc3e7785fd7b3466ad0081629bec87d9b839c1
Dana Shafer <shafer.dana@gmail.com>
2025-11-28 08:51:28 +0200

    feat(itemDisplayCard.tsx): add design for ledo card display

--- LegoStore/frontend/src/components/ItemDisplay.tsx/ItemDisplayCard.tsx
+++ LegoStore/frontend/src/components/ItemDisplay.tsx/ItemDisplayCard.tsx
@@ -2,17 +2,16 @@ import { FC } from "react";
 
 export const ItemDisplayCard: FC = () => {
   return (
-    <div className="card" style={{ width: 18 }}>
-      <img className="card-img-top" src="..." alt="Card image cap"></img>
-      <div className="card-body" style={{backgroundColor: "FFEDF4"}}>
+    <div className="card m-3 p-2" style={{ width: 200, backgroundColor: "#FFEDF4", border: "3px solid #ffcce1"}}>
+      <img className="card-img-top" src="..." style={{width: 180 , height: 180, borderRadius:10}}></img>
+      <div className="card-body">
         <h5 className="card-title">Lego name</h5>
-        <p className="card-text">
-         lego description
-        </p>
-        <a href="#" className="btn btn-primary">
+        <p className="card-text">lego description</p>
+        <a href="#" className="btn" style= {{backgroundColor: "#ffcce1"}}>
           add to cart
         </a>
       </div>
+ 
     </div>
   );
 };
