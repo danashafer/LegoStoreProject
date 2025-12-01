e1844c5bd5c34f581e5aefb8444223400d32dca5
Dana Shafer <shafer.dana@gmail.com>
2025-12-01 17:09:06 +0200

    feat(user.controller.ts): Add user controller

--- LegoStore/backend/src/controllers/lego.controller.ts
+++ LegoStore/backend/src/controllers/lego.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { LegoService } from 'src/services/lego.service';
 import { Lego } from 'src/entities/Lego.entity';
 
