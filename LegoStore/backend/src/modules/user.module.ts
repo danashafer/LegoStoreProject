f3cf9ddb5dae0b5c2456835ff6704c155f29d943
Dana Shafer <shafer.dana@gmail.com>
2025-12-02 17:35:30 +0200

    feat(app.module.ts): Add connection to database

--- LegoStore/backend/src/modules/user.module.ts
+++ LegoStore/backend/src/modules/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from 'src/controllers/user.controller';
+import { User } from 'src/entities/User.entity';
 import { UserService } from 'src/services/user.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
 })
