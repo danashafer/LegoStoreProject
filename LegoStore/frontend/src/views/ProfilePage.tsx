0121f65140f15afaedfea336dfc8106d3c92d9af
Dana Shafer <shafer.dana@gmail.com>
2025-12-02 11:16:33 +0200

    feat(ProfilePage.tsx): add design for viewing the profile

--- LegoStore/frontend/src/views/ProfilePage.tsx
+++ LegoStore/frontend/src/views/ProfilePage.tsx
@@ -1,10 +1,24 @@
-export const Profile =() => {
-    return (
-        <>
-        <h1>user name</h1>
-        <h1> order history </h1>
-        <i className="bi bi-clock-history"></i>
-        <h1> ongoing order status</h1>
-        </>
-    )
-}
+export const Profile = () => {
+  return (
+    <>
+    <div className="d-flex justify-content-center">
+      <div className="m-3 position-relative  " style={{width: 800}}>
+        <img src=".././assets/images/backgroundLego.png" alt=""  className="w-100"/>
+        <div className="position-absolute top-50 start-50 translate-middle bg-light rounded w-75 h-75">
+            <img src="" alt="" className="rounded-circle m-2 border border-seconsary" style={{height: 200, width: 200}}/>
+          <p className="fw-bold">user name</p>
+           <p>user email</p>
+
+           <div className="bg-secondary w-25 position-absolute start-50 translate-middle mt-5 rounded ">
+            <p> view order history</p>
+
+           </div>
+
+          
+          <i className="bi bi-clock-history"></i>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
