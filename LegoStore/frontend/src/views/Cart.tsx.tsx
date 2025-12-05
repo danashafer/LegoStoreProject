06b7b8b74ce4e3985ab60972f6f1a7f9a8936da5
Dana Shafer <shafer.dana@gmail.com>
2025-12-05 15:18:43 +0200

    feat(Cart.tsx): add cart page display

--- LegoStore/frontend/src/views/Cart.tsx.tsx
+++ LegoStore/frontend/src/views/Cart.tsx.tsx
@@ -13,5 +13,40 @@ export const Cart = () => {
     }
   }, [user, navigate]);
 
-  return <h1> this is the cart page </h1>;
+  return (
+
+    <>
+      <div className="d-flex justify-content-center">
+        <div className="m-3 position-relative  " style={{ width: 2000 }}>
+          <img
+            src=".././assets/images/backgroundLegoBig.png"
+            alt=""
+            className="w-100"
+          />
+          <div className="position-absolute top-50 start-50 translate-middle bg-light rounded w-75 h-75">
+            <h1> your cart</h1>
+            <div>
+              
+            </div>
+
+            {/* <img
+              src=""
+              alt=""
+              className="rounded-circle m-2 border border-seconsary"
+              style={{ height: 200, width: 200 }}
+            />
+            <p className="fw-bold">user name</p>
+            <p>{user?.email}</p>
+
+            <div className="bg-secondary w-25 position-absolute start-50 translate-middle mt-3g rounded ">
+              <p> view order history</p>
+            </div>
+
+            <i className="bi bi-clock-history"></i> */}
+          </div>
+        </div>
+      </div>
+    </>
+  // <h1> this is the cart page </h1>
+  )
 };
