ff6b3e4788b8665c8ba81e15f953a07dc6520f6d
Dana Shafer <shafer.dana@gmail.com>
2025-12-06 08:04:43 +0200

    feat(order.controller.ts): create file, add getById, AddLegoToOrder, DeleteLegoFromOrder functions

---
+++ LegoStore/backend/src/orders/order.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { Lego } from 'src/lego/Lego.entity';
+import { CartService } from 'src/cart/cart.service';
+
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getByUserId(@Req() req): Promise<Lego[]> {
+    const legosInOrder = await this.orderService.getByUserId(req.user.id);
+    console.log(legosInOrder);
+    return legosInOrder;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/:legoId')
+  async addNewLegoToOrder(
+    @Req() req,
+    @Param('legoId', ParseIntPipe) legoId: number,
+  ): Promise<void> {
+    console.log('adding to cart');
+    await this.orderService.addNewLegoToOrder(req.user.id, legoId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('/:legoId')
+  deleteLegoFromOrder(
+    @Req() req,
+    @Param('legoId', ParseIntPipe) legoId: number,
+  ): Promise<void> {
+    return this.orderService.deleteLegoFromOrder(req.user.id, legoId);
+  }
+}
