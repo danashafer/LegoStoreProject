ec5e382bf218a37caf389bf809ec2c9d965276ac
Dana Shafer <shafer.dana@gmail.com>
2025-12-08 12:50:05 +0200

    feat(uploads.controller.ts): add GetUserAvatarUploadUrl function

--- LegoStore/backend/src/uploads/upload.controller.ts
+++ LegoStore/backend/src/uploads/upload.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { LegoUploadDto } from './dto/lego-upload.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
+import { UserAvatarUploadDto } from './dto/avatar-upload.dto';
 
 @Controller('uploads')
 export class UploadController {
@@ -10,5 +12,16 @@ export class UploadController {
   async getLegoImageUploadUrl(@Body() body: LegoUploadDto) {
     const { legoId, fileName, fileType } = body;
     return this.uploadService.getLegoImageUploadUrl(legoId, fileName, fileType);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('user-avatar')
+  async getUserAvatarUploadUrl(@Req() req, @Body() body: UserAvatarUploadDto) {
+    const userId = req.user.userId; // adapt to your payload
+    return this.uploadService.getUserAvatarUploadUrl(
+      userId,
+      body.fileName,
+      body.fileType,
+    );
   }
 }
