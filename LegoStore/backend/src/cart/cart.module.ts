564ec710e6b3965ea49d433a4b9ed69beb8c1272
Dana Shafer <shafer.dana@gmail.com>
2025-12-08 23:35:57 +0200

    feat(cart.module.ts, order.module.ts): Fix entity to cartItems

--- LegoStore/backend/src/cart/cart.module.ts
+++ LegoStore/backend/src/cart/cart.module.ts
@@ -5,9 +5,10 @@ import { Cart } from 'src/cart/Cart.entity';
 import { Lego } from 'src/lego/Lego.entity';
 import { User } from 'src/users/User.entity';
 import { CartService } from 'src/cart/cart.service';
+import { CartItem } from './CartItem.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart, Lego, User])],
+  imports: [TypeOrmModule.forFeature([Cart, Lego, User, CartItem])],
   controllers: [CartControler],
   providers: [CartService],
 })
